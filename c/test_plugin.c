1c6843e459f71c753f78811c44b5b8769a12511d
Robert Dietrich <robert.dietrich@tu-dresden.de>
2019-11-29 15:18:36 +0100

    added test plugin and renamed setting StartRead to AlignRead

---
+++ c/test_plugin.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "config.h" // sets several defines
+
+// headers required for collectd
+#include "collectd.h"
+#include "common.h" /* auxiliary functions */
+#include "plugin.h" /* plugin_register_*, plugin_dispatch_values */
+
+#define PLUGIN_NAME "testplugin"
+
+static int test_plugin_read_zwei(void) {
+  INFO(PLUGIN_NAME ": %s:%d", __FUNCTION__, __LINE__);
+}
+
+static int test_plugin_read(void) {  
+  INFO(PLUGIN_NAME ": %s:%d", __FUNCTION__, __LINE__);
+
+  return 0;
+}
+
+static int test_plugin_init(void)
+{
+  INFO(PLUGIN_NAME ": %s:%d", __FUNCTION__, __LINE__);
+  
+  return 0;
+}
+
+static int test_plugin_flush(cdtime_t timeout, const char *identifier, user_data_t *usr )
+{
+  INFO(PLUGIN_NAME ": %s:%d", __FUNCTION__, __LINE__);
+  
+  return 0;
+}
+
+/*! brief Resets the test group counters
+
+Example notification on command line:
+echo "PUTNOTIF severity=okay time=$(date +%s) message=resetLikwidCounters" |   socat - UNIX-CLIENT:$HOME/sw/collectd/collectd-unixsock
+ */
+static int test_plugin_notify(const notification_t *type, user_data_t *usr )
+{
+  INFO (PLUGIN_NAME ": %s:%d", __FUNCTION__, __LINE__);
+  
+  return 0;
+}
+
+static int test_plugin_finalize( void )
+{
+  INFO (PLUGIN_NAME ": %s:%d", __FUNCTION__, __LINE__);
+
+  return 0;
+}
+
+static const char *config_keys[] =
+{
+  "verbose"
+};
+static int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
+
+static int test_plugin_config (const char *key, const char *value)
+{
+  INFO (PLUGIN_NAME " config: %s := %s", key, value);
+  
+  return 0;
+}
+
+/*
+ * This function is called after loading the plugin to register it with collectd.
+ */
+void module_register(void) {
+  plugin_register_config (PLUGIN_NAME, test_plugin_config, config_keys, config_keys_num);
+  plugin_register_read(PLUGIN_NAME, test_plugin_read);
+  plugin_register_read("zwei", test_plugin_read_zwei);
+  plugin_register_init(PLUGIN_NAME, test_plugin_init);
+  plugin_register_shutdown(PLUGIN_NAME, test_plugin_finalize);
+  plugin_register_flush(PLUGIN_NAME, test_plugin_flush, /* user data = */ NULL);
+  plugin_register_notification(PLUGIN_NAME, test_plugin_notify, /* user data = */ NULL);
+  return;
+}
