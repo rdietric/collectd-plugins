431875bb1f3542f26796d036b326df7e00303b90
Robert Dietrich <robert.dietrich@tu-dresden.de>
2019-12-20 13:20:38 +0100

    added option to write rates for derive and counter types

--- python/influx_write.py
+++ python/influx_write.py
@@ -3,6 +3,17 @@
 """
 Send metrics to InfluxDB (https://github.com/influxdb/influxdb/) using the
 InfluxDBClient interface.
+
+Collectd Values are sent/mapped to InfluxDB as follows:
+measurement <- plugin
+field/metric name <- type instance, if available, otherwise type
+tag name/type (metric specific) <- either the plugin name or 'cpu', if the 
+                                   plugin ends with 'cpu' or '_socket'
+tag value (metric specific) <- plugin instance
+
+Additionally, the host name is written as tag for 'hostname'.
+
+A collectd value is identified by plugin, plugin instance, type and type instance.
 """
 
 import collectd
@@ -18,26 +29,35 @@ except ImportError:
 
 influx = None
 
+ssl = False
 hostname = 'localhost'
 port = 8086
-username = 'user'
-password = 'pwd'
-database = 'db'
-divide_cpu_used_by_100 = False
+username = None
+password = None
+database = None # name of the database
 
 batch_count = 0
-batch_size = 200
-cache_size = 2000
-batch = {}
+batch_size = 200   # number of metrics to be sent in one batch
+cache_size = 2000  # maximum number of metrics to store locally (e.g. if sends fail)
+batch = {} # all unsent value lists are stored here
+
+store_rates = False
+batch_derive = {} # all previous value lists of derived/counter types are stored here
 
 debug = True
 time_precision = 's'
 
+"""
+Set plugin configuration (from collectd config file).
+"""
 def set_config(config):
   if config.values[0] == 'influx_write':
     collectd.info("[InfluxDB Writer] Get configuration")
     for value in config.children:
-      if value.key == 'host':
+      if value.key == 'ssl':
+        global ssl
+        ssl = bool(value.values[0])
+      elif value.key == 'host':
         global hostname
         hostname = value.values[0]
       elif value.key == 'port':
@@ -49,17 +69,32 @@ def set_config(config):
       elif value.key == 'pwd':
         global password
         password = value.values[0]
+      elif value.key == 'database':
+        global database
+        database = value.values[0]
+      elif value.key == 'batch_size': 
+        global batch_size
+        batch_size = _getInteger(value.values[0])
+      elif value.key == 'cache_size':
+        global cache_size
+        cache_size = _getInteger(value.values[0])
+      elif value.key == 'StoreRates':
+        global store_rates
+        store_rates = value.values[0]
+        if store_rates:
+          collectd.info("[InfluxDB Writer] Store rates for derived and counter types")
+      else:
+        collectd.info("[InfluxDB Writer] Ignore unknown option %s" % (value.key,))
 
-
+"""
+Connect to the InfluxDB server
+"""
 def _connect():
-  """
-  Connect to the InfluxDB server
-  """
   try:
       # Open Connection
       global influx
       influx = InfluxDBClient(host=hostname, port=port, username=username, 
-                              password=password, database=database)
+                              password=password, database=database, ssl=ssl)
       
       collectd.info("[InfluxDB Writer] Established connection to %s:%d/%s." % (hostname, port, database) )
   except Exception as ex:
@@ -67,35 +102,32 @@ def _connect():
       collectd.info("[InfluxDB Writer] Failed to connect to %s:%s/%s. (%s:%s) - %s" % (hostname, port, database, username, password, ex) )
       _close()
 
+"""
+Close the socket = do nothing for influx which is http stateless
+"""
 def _close():
-    """
-    Close the socket = do nothing for influx which is http stateless
-    """
     global influx
     influx = None
 
+"""
+Collectd initialization callback.
+Responsible for starting the sending thread
+"""
 def init_callback():
-  """
-  Collectd initialization callback.
-  Responsible for starting the sending thread
-  """
-
   global InfluxDBClient
   if not InfluxDBClient:
     collectd.info('[InfluxDB Writer] influxdb.client.InfluxDBClient import failed.')
   else:
-    collectd.info('[InfluxDB Writer] Initialize.')
+    #collectd.info('[InfluxDB Writer] Initialize.')
     _connect()
   
 def write(valueList, data=None):
   if not InfluxDBClient:
     return
 
-  #print "thread:", threading.currentThread().ident
-
-  collectd.info('[InfluxDB Writer] %s' % (str(valueList),))
-  if data:
-    collectd.info('[InfluxDB Writer] Data: %s' % (str(data),))
+  #collectd.info('[InfluxDB Writer] %s' % (str(valueList),))
+  #if data:
+  #  collectd.info('[InfluxDB Writer] Data: %s' % (str(data),))
 
   global batch
   global batch_count
@@ -111,24 +143,8 @@ def write(valueList, data=None):
     _send()
 
 """
-Store values per per plugin. This enables to send a point with multiple values.
-"""
-def _store_per_plugin(valueList):
-  global batch
-
-  if valueList.plugin:
-    plugin_name = valueList.plugin
-  else:
-    plugin_name = valueList.type
-
-  # create array for collector, if it is not available yet
-  if plugin_name not in batch:
-    batch[plugin_name] = []
-
-  batch[plugin_name].append(valueList)
-
-"""
-Store values per per plugin[instance]. This enables to send a point with multiple values/fields.
+Store values per per plugin instance. 
+Plugin name and plugin instance (tag) identify a value list. 
 """
 def _store_per_plugin_instance(valueList):
   global batch
@@ -141,13 +157,9 @@ def _store_per_plugin_instance(valueList):
     collectd.error('[InfluxDB Writer] Either a plugin or type is required!')
     return
 
-  #tag = None
-  # plugin instance is used as tag
-  #if valueList.plugin_instance:
   tag = valueList.plugin_instance
 
-  #if valueList.plugin.endswith('cpu') or valueList.plugin.endswith('_socket'): 
-  #  tag = _getInteger(tag)
+  #collectd.info("Write valueList: %s" % (valueList,))
 
   # create array for plugin and tag, if it is not available yet
   if plugin_name in batch:
@@ -165,19 +177,26 @@ def _store_per_plugin_instance(valueList):
 Send data to InfluxDB. Data that cannot be sent will be kept in cache.
 """
 def _send():
+  global batch
+  global batch_count
+
   if not influx:
     collectd.info('[InfluxDB Writer] Connection not available. Try reconnect ...')
     _connect()
 
-  #metrics = _send_multiple_values_per_line()
-  metrics = _send_new()
+  metrics = _prepare_metrics()
 
+  # reset batch which only contains initial values of derived metrics
   if len(metrics) == 0:
+    batch = {}
+    batch_count = 0
+    if len(batch_derive) == 0:
+      collectd.info('[InfluxDB Writer] No metrics to send. '
+        'No previous values are stored. Should not happen!')
     return
 
   # Send data to influxdb
-  if debug:
-    collectd.info('[InfluxDB Writer] Write %d series of data' % (len(metrics)))
+  collectd.debug('[InfluxDB Writer] Write %d series of data' % (len(metrics)))
 
   ret = False
 
@@ -190,24 +209,24 @@ def _send():
 
   # empty batch buffer for successful writes
   if ret:
-    global batch
-    global batch_count
-
+    #collectd.info("reset batch")
     batch = {}
     batch_count = 0
 
-def _send_new():
+def _prepare_metrics():
   global batch
 
   # build metrics data
   metrics = []
-  for measurement in batch.keys():
+  for measurement in batch:
     for tag in batch[measurement]:
       last_time = -1
       fields = {}
 
       # iterate over the value lists
       for valueList in batch[measurement][tag]:
+        counterMetricID = None # default is a gauge metric type, no metric ID needed
+
         time = int(valueList.time)
 
         # if the tag (plugin instance) is not None, add it with measurement (plugin) as key
@@ -225,29 +244,63 @@ def _send_new():
           #metricName = "value"
         field_name = metricName
         
-        #collectd.info("Metric: %s" % (str(metric),))
         if len(valueList.values) == 0:
           collectd.info("No values available for %s:%s!" % (measurement,metricName))
           continue
 
         for midx, value in enumerate(valueList.values):
           # ignore invalid values
-          if str(value) == "nan" and math.isnan(float(value)) and str(value) == "inf":
+          if str(value) == "nan" or math.isnan(float(value)) or str(value) == "inf":
             continue
+
+          # get dataset to determine types
+          ds = collectd.get_dataset(valueList.type)
 
           # get metric name from data type, if we have more than one value
           if len(valueList.values) > 1:
-            type_info = collectd.get_dataset(valueList.type) 
-            #if type_info:
-            #  print type_info
             try:
-              # prepend value type
-              field_name = type_info[midx][0] + "_" + metricName
+              # prepend type name
+              field_name = ds[midx][0] + "_" + metricName
             except:
               field_name = metricName + str(midx)
 
+          # for derived counters
+          if store_rates and (ds[midx][1] == 'derive' or ds[midx][1] == 'counter'):
+            #collectd.info("Derived: %s (%s)" % (valueList,ds))
+
+            # determine metric identifier from mandatory and optional values
+            if counterMetricID == None:
+              counterMetricID = valueList.plugin+valueList.type # mandatory identifier
+              # optional identifier
+              if valueList.plugin_instance:
+                counterMetricID += valueList.plugin_instance
+              if valueList.type_instance:
+                counterMetricID += valueList.type_instance
+
+            #collectd.info(metric_key)
+            if counterMetricID in batch_derive:
+              prevValueList = batch_derive[counterMetricID]
+              diff_time = time - int(prevValueList.time)
+              if diff_time > 0:
+                # determine the rate
+                diff_value = value - prevValueList.values[midx]
+                value = float(diff_value) / float(diff_time)
+
+                #collectd.info("[InfluxDB Writer] %s: Rate: %f" % (measurement+"_"+tag+"_"+field_name, value) )
+              else:
+                # can occur, if we have the same plugin and plugin instance,
+                # but different types (e.g. with the disk plugin)
+                if prevValueList.type == valueList.type:
+                  collectd.warning("[InfluxDB Writer] Found a previous value "
+                    "for this metric with the same timestamp (prev: %s, curr: %s)"
+                    % (batch_derive[counterMetricID], valueList) )
+                continue
+            else:
+              continue
+
           # if possible, write all fields in a single line
           # if next value has the same timestamp, add it as another field
+          # works only, if different fields/values are read within the same second
           if time == last_time:
             # add field values to measurement point
             fields[field_name] = value
@@ -260,7 +313,7 @@ def _send_new():
                   "tags": tags,
                   "fields": fields}
                   )
-              collectd.info("Data point: %s:%d, tags: %s, fields: %s" % (measurement, int(last_time), str(tags), str(fields)))
+              #collectd.info("Data point: %s:%d, tags: %s, fields: %s" % (measurement, int(last_time), str(tags), str(fields)))
 
             # write first field value for next measurement point
             fields = {field_name: value}
@@ -268,56 +321,19 @@ def _send_new():
           # remember last timestamp
           last_time = time
 
+        # store values to determine rates
+        if counterMetricID:
+          batch_derive[counterMetricID] = valueList
+
       # write remaining fields
-      if fields: 
-        collectd.info("Remaining data point: %s:%d, tags: %s, fields: %s" % (measurement, int(last_time), str(tags), str(fields)))
+      if fields:
+        #collectd.info("Remaining data point: %s:%d, tags: %s, fields: %s" % (measurement, int(last_time), str(tags), str(fields)))
         metrics.append({
             "measurement": measurement,
             "time": last_time,
             "tags": tags,
             "fields": fields}
             )
-
-  return metrics
-
-def _send_one_value_per_line():
-  global batch
-
-  # build metrics data
-  metrics = []
-  for measurement in batch.keys():
-    for value_obj in batch[measurement]:
-      time = int(value_obj.time)
-      tags = {"hostname": value_obj.host}
-
-      # the plugin field contains the measurement name
-      if value_obj.plugin:
-        mname = value_obj.plugin
-      else:
-        mname = value_obj.type
-
-      # if plugin instance is given, interpret it as CPU number
-      if value_obj.plugin_instance:
-        tags['cpu'] = int(value_obj.plugin_instance)
-      
-      # the type instance contains the field/metric name
-      if value_obj.type_instance:
-        field_name = value_obj.type_instance
-      else:
-        field_name = 'value'
-
-      for value in value_obj.values:
-        if str(value) == "nan" or math.isnan(float(value)):
-          value = 0
-
-        #collectd.info("%s:%s %f at %s, host %s" % (value_obj.plugin, value_obj.type_instance, value, value_obj.time, value_obj.host))
-
-        metrics.append({
-          "measurement": mname,
-          "time": time,
-          "tags": tags,
-          "fields": {field_name: value}}
-        )
 
   return metrics
 
@@ -332,134 +348,6 @@ def _getInteger(stringInt):
       return int(ret.group())
     
   return ret
-
-"""
-Wrap multiple values in a single send, if tags (time, cpu, hostname) do not change.
-"""
-def _send_multiple_values_per_line():
-  global batch
-
-  metrics = [] # new metrics array
-  for measurement in batch.keys():
-    last_valueList = None
-    last_cpu = ""
-    last_gpu = ""
-    fields = {}
-    tags = {}
-    last_tags = {}
-    for valueList in batch[measurement]:
-      tags = {"hostname": valueList.host}
-      time = valueList.time
-
-      # determine metric name
-      metricName = valueList.type_instance
-      if metricName is None or metricName == '':
-        metricName = "value"
-      field_name = metricName
-
-      # if there is a plugin_instance, we add it as tag with plugin name as tag name
-      if valueList.plugin_instance and valueList.plugin:
-        tags[valueList.plugin] = valueList.plugin_instance
-
-      
-      #collectd.info("Metric: %s" % (str(metric),))
-      if len(valueList.values) == 0:
-        collectd.info("No values available for %s:%s!" % (measurement,metricName))
-        continue
-
-      for midx, value in enumerate(valueList.values):
-        # ignore invalid values
-        if str(value) == "nan" and math.isnan(float(value)) and str(value) == "inf":
-          continue
-
-        # get metric name from data type, if we have more than one value
-        if len(valueList.values) > 1:
-          type_info = collectd.get_dataset(valueList.type) # not implemented for python
-          #if type_info:
-          #  print type_info
-          try:
-            # prepend value type
-            field_name = type_info[midx][0] + "_" + metricName
-          except:
-            field_name = metricName + str(midx)
-
-        # for measurements with values per CPU
-        # endswith('cpu') covers CPU usage collector and likwid per cpu metrics
-        cpu = ""
-        if measurement.endswith('cpu') or measurement.endswith('_socket'): 
-          # generate cpu tag from last value (compare last timestamp)
-          tags["cpu"] = last_cpu # add CPU number as tag
-          # add CPU number as tag 
-          if valueList.plugin_instance:
-            cpu = _getInteger(valueList.plugin_instance)
-
-        # for measurements with values per GPU
-        gpu = ""
-        if field_name.startswith('gpu#'):
-          # split metric name
-          mname_list = field_name.split('.')
-          
-          # remove gpu#* prefix from metric name
-          gpu = mname_list.pop(0)
-          
-          # generate GPU ID tag
-          if last_gpu != "":
-              tags["gpu"] = last_gpu.replace("gpu#","")
-          else:
-              tags["gpu"] = gpu.replace("gpu#","")
-          
-          if mname_list > 1:
-              field_name = ".".join(mname_list)
-          else:
-              field_name = mname_list[0]
-
-        # if possible, write all fields in a single line
-        # if next value has the same timestamp and GPU, add it as another field
-        if last_valueList and time == last_valueList.time and cpu == last_cpu and gpu == last_gpu:
-          # add field values to measurement point
-          fields[field_name] = value
-        else:
-          if fields: # fields are available, but time changed
-            # write last fields with last timestamp
-            last_tags = {"hostname": last_valueList.host}
-            if last_gpu != "":
-              last_tags["gpu"] = last_gpu.replace("gpu#","")
-            if last_cpu != "":
-              last_tags["cpu"] = last_cpu
-              
-            #collectd.info("Data point: %s:%d, tags: %s, fields: %s" % (measurement, int(last_valueList.time), str(last_tags), str(fields)))
-            
-            metrics.append({
-                "measurement": measurement,
-                "time": int(last_valueList.time),
-                "tags": last_tags,
-                "fields": fields}
-                )
-
-          # write first field value for next measurement point
-          fields = {field_name: value}
-
-        # remember last metric values
-        last_cpu = cpu
-        last_gpu = gpu
-        last_valueList = valueList
-
-    # write remaining fields
-    if fields:
-      if "gpu" in tags:
-        tags["gpu"] = last_gpu.replace("gpu#","")
-      #elif 'cpu' in tags:
-      #  tags["cpu"] = last_cpu.replace("cpu","")
-        
-      #collectd.info("Remaining data point: %s:%d, tags: %s, fields: %s" % (measurement, int(last_valueList.time), str(tags), str(fields)))
-      metrics.append({
-          "measurement": measurement,
-          "time": int(last_valueList.time),
-          "tags": tags,
-          "fields": fields}
-          )
-
-  return metrics
   
 def flush(timeout, identifier):
   global batch_count
